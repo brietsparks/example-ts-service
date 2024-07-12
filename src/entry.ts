83959d619e287d81b9a1aaffffdac5d102e4f690
Briet Sparks <brietsparks@gmail.com>
2024-07-11 20:39:20 -0500

    init

---
+++ src/entry.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { config as configEnv } from 'dotenv';
+
+configEnv();
+
+const port = process.env.PORT;
+if (!port) {
+  throw new Error('PORT is missing')
+}
+
+const exampleVar = process.env.EXAMPLE_VAR;
+if (!exampleVar) {
+  throw new Error('EXAMPLE_VAR is missing');
+}
+
+const server = express();
+
+server.get('/hello/:name', (req, res) => {
+  const name = req.params.name;
+  res.json({ message: `Hello, ${name}!` });
+});
+
+server.get('/example', (_, res) => {
+  res.json(exampleVar);
+});
+
+server.listen(port, () => console.log(`listening on port ${port}`));
